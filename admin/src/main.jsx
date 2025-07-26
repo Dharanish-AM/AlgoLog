33642faa0d235e7515488e8ba9a07cb5b4afe51b
Dharanish A M <dharanish816@gmail.com>
2025-07-26 09:17:15 +0530

    Fix: Ensure React 18 and Vite plugin for JSX runtime

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
-import { ThemeProvider } from "../../faculty/src/context/ThemeContext.jsx";
+import { ThemeProvider } from "../context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
